1a79d0510f72d641d65bd812c1d8e63688dc984f
Harsh1jatt <148642923+Harsh1jatt@users.noreply.github.com>
2024-11-15 11:52:29 +0530

    Update typingTestModel.js

--- models/typingTestModel.js
+++ models/typingTestModel.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 const TypingTestSchema = new mongoose.Schema({
-    institute: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Institute',
-        required: true,
-    },
     exam: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Exam',
